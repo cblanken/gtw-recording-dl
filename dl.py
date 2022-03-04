75ca0623ed88df9e9b787ce641033909f3a2cb34
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-03-04 00:37:17 -0500

    Add single GoToWebinar recording link download functionality
    Use the requests library to grab recording id and post
    registration info to get direct CDN link to recording
    and download with wget.

---
+++ dl.py
@@ -0,0 +1,71 @@
+from sys import argv
+from wget import download
+import requests
+import json
+
+if __name__ == "__main__":
+    if len(argv) != 3:
+        print("Usage: python dl.py <GoToWebinar link> <output file>")
+    else:
+        page_url = str(argv[1])
+        recording_id = page_url.split("/")[-1]
+        print("Recording ID:", recording_id)
+        page = requests.get(page_url)
+
+        headers1 = {
+            "Host": "globalattspa.gotowebinar.com",
+            "Accept": "*/*",
+            "Access-Control-Request-Method": "POST",
+            "Access-Control-Request-Headers": "content-type",
+            "Origin": "https://register.gotowebinar.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-site",
+            "Referer": "https://register.gotowebinar.com/",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "close",
+        }
+
+        headers2 = {
+            "Host": "globalattspa.gotowebinar.com",
+            # "Content-Length":"99",
+            "Sec-Ch-Ua": '"Chromium";v="93", " Not;A Brand";v="99"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
+            "Sec-Ch-Ua-Platform": '"Linux"',
+            "Content-Type": "application/json",
+            "Accept": "*/*",
+            "Origin": "https://register.gotowebinar.com",
+            "Sec-Fetch-Site": "same-site",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Dest": "empty",
+            "Referer": "http://register.gotowebinar.com/",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Connection": "close",
+        }
+
+        # Provide registration info, name and email, to api
+        # TODO read creds from JSON file or from command line
+        reg_info = {"firstName":"Cameron","lastName":"Blankenbuehler","email":"cameron.blankenbuehler@protonmail.com"}
+        recording_page_url = "https://globalattspa.gotowebinar.com/api/recordings/" + recording_id + "/registrants"
+
+        # Send OPTIONS request to allow registration
+        r1 = requests.options(url=recording_page_url, headers=headers1)
+
+        # Send POST with registration creds
+        r2 = requests.post(url=recording_page_url, data=json.dumps(reg_info), headers=headers2)
+
+        registrant_key = str(r2.json()["registrantKey"])
+        print("Registrant Key:", registrant_key)
+
+        # Send GET to retrieve CDN link to video file 
+        get_vid_url = "https://api.services.gotomeeting.com/registrationservice/api/v1/webinars/{0}/registrants/{1}/recordingAssets?type=OFFLINEWEBINAR&client=spa".format(recording_id, registrant_key)
+        r3 = requests.get(url=get_vid_url, headers=headers2)
+        vid_url = str(r3.json()["cdnLocation"])
+        
+        # Download recording with wget
+        download(vid_url, out=argv[2])
+        print()
+
